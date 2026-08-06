f51b7ecbe108dc208ee36617204ef856ac5ce3eb
Dhwnit27 <solankidhwnit1@gmail.com>
2026-08-06 18:10:40 +0530

    Fixing problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,6 @@ import HampiPage from "./pages/HampiPage";
 
 import Features from "./components/Features";
 import About from "./components/About";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
